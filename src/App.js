f9268b566448c58b942099df0760530fe5d5d704
Dan Seaman <danseaman6@gmail.com>
2018-06-10 20:48:53 -0400

    added headshot image

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import React, { Component } from "react";
-import logo from "./images/logo.svg";
+import headshot from "./images/headshot.JPG";
 import "./css/App.css";
-import {Button}  from 'react-bootstrap';
-import Demo from "demo";
+import {Button, Grid, Row, Col}  from 'react-bootstrap';
+import Demo from "./demo";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <img src={headshot} className="App-headshot" alt="logo" />
+          <h1 className="App-title">Dan Seaman</h1>
+          <h2>Software Engineer</h2>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>You know the drill: HELLO, WORLD! (TEST)</p>
-        <Button disabled={true}>
-          This is a demo
-        </Button>
-        <Demo dstring="try this stateless component"/>
+        <Demo dstring="filler"/>
       </div>
     );
   }
